f09de55b74a3490f5262a1c9010c7138960382bf
Yang Chung <yangtheman@gmail.com>
2014-04-05 15:00:39 -0700

    Added comments

--- lib/custom_sort.rb
+++ lib/custom_sort.rb
@@ -8,6 +8,9 @@ class CustomSort
     @base = base
   end
 
+  # Nested loop. Inspect each element of outer loop with all elements of inner
+  # loop. If smaller element is found, swap it immediately.
+  # O(n^2)
   def bubble_sort(array=@base)
     array.each_index do |index1|
       index1.upto(array.size-2) do |index2|
@@ -19,6 +22,9 @@ class CustomSort
     array
   end
 
+  # Nested loop. Inspect each element of outer loop with all elements of inner
+  # loop. If smaller element is found, store index of the element. Swap it at
+  # the end of each inner loop iteration.
   def selection_sort(array=@base)
     0.upto(array.size-1) do |index1|
       min_index = index1
@@ -30,6 +36,8 @@ class CustomSort
     array
   end
 
+  # Merge divided arrays. Iterate through two arrays, compare first elements
+  # of arrays, and remove the smaller item.
   def merge_sort_merge(left, right)
     sorted = []
     until left.empty? || right.empty?
@@ -43,6 +51,8 @@ class CustomSort
     sorted
   end
 
+  # Divide given array into two equal-length arrays. Return the array if it only
+  # contains one element.
   def merge_sort(array=@base)
     return array if array.size < 2
     mid = array.size/2
@@ -54,6 +64,8 @@ class CustomSort
     merge_sort_merge(left_merge, right_merge)
   end
 
+  # Take first element as pivot and divide them into two arrays, one with bigger
+  # elements and the other with smaller elements than the pivot element.
   def quick_sort(array=@base)
     return [] if array.empty?
     pivot = array.pop
@@ -61,6 +73,10 @@ class CustomSort
     quick_sort(left) + [pivot] + quick_sort(right)
   end
 
+  # Start from second element and iterate through the array in the outer loop.
+  # Inner loop goes from previous element to the beginning of the array. Move
+  # element to one position right if it's bigger than the element from the outer
+  # loop.
   def insertion_sort(array=@base)
     1.upto(array.size-1) do |index1|
       num = array[index1]
