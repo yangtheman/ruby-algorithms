59d1d89a4589b99a6ae935e68f0027c813203b6e
Yang Chung <yangtheman@gmail.com>
2014-04-10 22:00:00 -0700

    finish string compress

--- lib/ctci/chapter_1/string_compress.rb
+++ lib/ctci/chapter_1/string_compress.rb
@@ -1,28 +1,37 @@
 require 'rspec'
 
+def combine_str_and_num(string, num)
+  num > 1 ? string + num.to_s : string
+end
+
 def string_compress(str)
   splited = str.split("")
   current_char = splited.shift
   char_num = 1
-  compressed = current_char
-  splited.each_with_index do |char, index|
+  compressed = ""
+  splited.each do |char|
     if char != current_char
-      if char_num > 1
-        compressed << char + char_num.to_s
-      else
-        compressed << char
-      end
+      compressed << combine_str_and_num(current_char, char_num)
       current_char = char
       char_num = 1
     else
       char_num += 1
     end
   end
-  compressed
+  compressed << combine_str_and_num(current_char, char_num)
 end
 
 describe "#string_compress" do
   it "should compress repeated chars" do
     string_compress("aabcc").should == "a2bc2"
   end
+
+  it "should compress repeated chars" do
+    string_compress("aabc").should == "a2bc"
+  end
+
+  it "should return given string if nothing repeats" do
+    string_compress("abcde").should == "abcde"
+  end
+
 end
